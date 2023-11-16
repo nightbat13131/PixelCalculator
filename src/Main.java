913d1991bd03f6dfe82306f4fd8540b1207ab49c
Cole H <nightbat13131@gmail.com>
2023-11-15 16:50:08 -0800

    initializing project

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // take tile size, rows count , colum count and return fill width and hight
+        // take row number and column number and return (inclusive) top left cord and bottom right cord
+        // also return top left cord with width and height (used in godot to identify region
+
+
+
+
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
