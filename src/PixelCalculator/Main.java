ecae04538b00f95bd1d12e075cf76169d1067706
Cole H <nightbat13131@gmail.com>
2023-11-24 12:04:31 -0800

    Learned: in addition to my trouble setting up this in GIT, I also branched incorrectly. Trying to force a merge...

--- src/PixelCalculator/Main.java
+++ src/PixelCalculator/Main.java
@@ -1,6 +1,5 @@
 package PixelCalculator;
 
-import javax.swing.*;
 import java.lang.Integer;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
